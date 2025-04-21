4beb4beb51f9fe8b06400defee34b6204a98e59e
ted mbang <tdark237@gmail.com>
2025-04-21 13:21:56 +0200

    created the content for the popover - setings button and drawer modal with form

--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,19 +1,65 @@
-import { Popover } from "antd";
+import { Button, Popover } from "antd";
 import CustomAvatar from '../custom-avatar'
 import { useGetIdentity } from "@refinedev/core";
 
 import type { User } from "@/graphql/schema.types";
+import { Text } from "../text";
+import { SettingOutlined } from "@ant-design/icons";
+import { useState } from "react";
+import { AccountSettings } from "../account-settings";
 
 const CurrentUser = () => {
 
+    const [isOpen, setIsOpen] = useState(false)
   const { data: user } = useGetIdentity<User>()
+
+  const content = (
+    <div
+      style={ {
+        display: 'flex',
+        flexDirection: 'column'
+      }}
+    >
+      <Text
+        strong
+        style={{ 
+          padding: '12px 20px'
+         }}
+      >
+        {user?.name}
+      </Text>
+      <div
+        style={{
+          border: '1px solid #d9d9d9',
+          padding: '4px',
+          flexDirection: 'column',
+          gap: '4px'
+        }}
+      >
+        <Button
+          style={{
+            textAlign: 'left'
+          }}
+          icon={
+            <SettingOutlined/>
+          }
+          type="text"
+          block
+          onClick={() => setIsOpen(true)}
+
+        >
+          Account Settings
+        </Button>
+      </div>
+    </div>
+  )
 
   return (
     <>
       <Popover
         placement="bottomRight"
         trigger="click"
-        content={<div style={{ padding: 0, zIndex: 999 }}>Test</div>}
+        content={content}
       >
         <CustomAvatar
           name={user?.name}
@@ -22,6 +68,13 @@ const CurrentUser = () => {
           style={{ cursor: 'pointer' }}
         />
       </Popover>
+      {user && (
+        <AccountSettings
+          opened={isOpen}
+          setOpened={setIsOpen}
+          userId={user.id}
+        />
+      )}
     </>
   );
 };
