f433138eda6cb158b259f0c9a4d470feba17fd2e
ted mbang <tdark237@gmail.com>
2025-04-21 11:44:14 +0200

    built the custom current user avatar component

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,22 +1,25 @@
 import { Avatar as AntdAvatar, AvatarProps } from 'antd'
+import { getNameInitials } from '@/utilities'
 
 type Props = AvatarProps & {
-    name: string;
+    name?: string;
 }
 
 const customAvatar = ({ name, style, ...rest }: Props) => {
   return (
     <AntdAvatar
-        alt={'Dark Teddy'}
+        alt={name}
         size={'small'}
         style={{ 
             backgroundColor: '#87d068', 
             display: 'flex',
             alignItems: 'center',
-            border: 'none'
+            border: 'none',
+            ...style
         }}
+        {...rest}
     >
-        {name}
+        {getNameInitials(name || '')}
     </AntdAvatar>
   )
 }
--- src/components/layout/current-user.tsx
+++ src/components/layout/current-user.tsx
@@ -1,11 +1,13 @@
-import { Popover, Button } from "antd";
+import { Popover } from "antd";
 import CustomAvatar from '../custom-avatar'
 import { useGetIdentity } from "@refinedev/core";
 
-import type { User } from '@/graphql/schema.type'
+import type { User } from "@/graphql/schema.types";
 
 const CurrentUser = () => {
+
   const { data: user } = useGetIdentity<User>()
+
   return (
     <>
       <Popover
@@ -13,7 +15,12 @@ const CurrentUser = () => {
         trigger="click"
         content={<div style={{ padding: 0, zIndex: 999 }}>Test</div>}
       >
-        <CustomAvatar name=""/>
+        <CustomAvatar
+          name={user?.name}
+          src={user?.avatarUrl}
+          size='default'
+          style={{ cursor: 'pointer' }}
+        />
       </Popover>
     </>
   );
